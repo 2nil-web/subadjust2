60c1906b6108b59bf3c6f51f26b9c0980a1090a5
2nil <casalalanne@free.fr>
2025-06-22 12:01:30 +0200

    Checked in by Denis from PC-Denis-2 on Sun, Jun 22, 2025 12:01:30 PM

--- head.js
+++ head.js
@@ -31,10 +31,10 @@ var file_content;
 
 
 function basename(path) {
-   return path.split(/[\\/]/).pop();
+  return path.split(/[\\/]/).pop();
 }
 
-var inc=0;
+var inc = 0;
 async function read_file(filename) {
   filename = decodeHtml(filename);
   //console.log("read_file: [" + filename + ']');
@@ -42,7 +42,7 @@ async function read_file(filename) {
   if (filename.length > 0) {
     // This test does not arm but seems to be unuseful under Windows and GTK
     if (await fs.exists(filename)) {
-      app.set_title(document.title+" - " + basename(filename));
+      app.set_title(document.title + " - " + basename(filename));
       filepath.value = filename;
       file_content.innerText = await fs.read(filename);
     } else {
@@ -55,52 +55,85 @@ async function load_file() {
   read_file(await gui.opendlg());
 }
 
-if (typeof app.sysname !== "undefined") {
-  //for (const key of Object.keys(localStorage)) { console.log(key+": "+localStorage.getItem(key)); }
-  //localStorage.clear();
-  // To try to juxtapose multiple subadjust instances
-  var bc = new BroadcastChannel(document.title);
+var max_width = 0,
+  max_height = 0;
+(async () => {
+  if (typeof app.sysname !== "undefined") {
+    /*
+    for (const key of Object.keys(localStorage))
+      console.log(key+": "+localStorage.getItem(key));
+    localStorage.clear();
+    */
 
-  app.set_icon("app.ico");
+    // Get the max width and height of the working area
+    mons = await win.monitors_info();
+    //console.log(`Monitors: ${JSON.stringify(mons)}`);
+    for (const [key, mon] of Object.entries(mons)) {
+      //console.log(`${key}: ${JSON.stringify(mon)}`);
+      working_area=mon['working area'];
+      //console.log(`${key}[working area]: ${JSON.stringify(working_area)}`);
+      if (working_area[2] > max_width)
+        max_width = working_area[2];
+      if (working_area[3] > max_height)
+        max_height = working_area[3];
+    }
+    console.log(`max_width=${max_width}, max_height=${max_height}`);
 
-  bc.onmessage = (event) => {
-    inc++;
-    //new_title=app.title.replace(/\d* - /, inc+" - ");
-    //app.set_title(new_title);
-    var newX=app.x+inc+(app.w-(app.left_border+app.right_border));
-    console.log(`new X: ${newX}`);
-    app.set_pos(newX, app.y);
-  }
+    // To try to juxtapose multiple subadjust instances
+    var bc = new BroadcastChannel(document.title);
 
-  if (inc === 0) {
-    // Tell the other pages I'm here
-    bc.postMessage('run_increment');
-  }
+    app.set_icon("app.ico");
 
-  args = app.args_line.split(',');
-  //for (var i = 0; i < args.length; i++) { console.log(`args[${i}]=${args[i]}`); }
-
-  window.addEventListener("load", do_load);
-
-  function clean_exit() {
-    console.log("clean_exit");
-    localStorage.setItem(document.title+"_incr", "0");
-    bc.close();
-    app.exit();
-  }
-
-  async function do_load() {
-    document.addEventListener("keyup", exit_on_esc);
-    await app.set_size(420, 600, 1);
-    await app.show();
-    file_content = document.getElementById("file_content");
-
-    if (args.length >= 2) {
-      read_file(args[1]);
+    bc.onmessage = (event) => {
+      inc++;
+      //new_title=app.title.replace(/\d* - /, inc+" - ");
+      //app.set_title(new_title);
+      var newX = app.x - (app.w - (app.left_border + app.right_border));
+      if (newX < 0) newX = 0;
+      console.log(`new X: ${newX}`);
+      app.set_pos(newX, app.y);
     }
 
+    if (inc === 0) {
+      // Tell the other pages I'm here
+      bc.postMessage('run_increment');
+    }
+
+    args = app.args_line.split(',');
+    //for (var i = 0; i < args.length; i++) { console.log(`args[${i}]=${args[i]}`); }
+
+    window.addEventListener("load", do_load);
+
+    function clean_exit() {
+      console.log("clean_exit");
+      localStorage.setItem(document.title + "_incr", "0");
+      bc.close();
+      app.exit();
+    }
+
+    async function do_load() {
+      document.addEventListener("keyup", exit_on_esc);
+      await app.set_size(420, 600, 1);
+      if (app.x < 0) correcX = 0;
+      else if (app.x > max_width) correcX = max_width;
+      else correcX = app.x;
+
+      if (app.y < 0) correcY = 0
+      else if (app.y > max_height) correcX = max_height;
+      else correcY = app.y;
+
+      app.set_pos(correcX, correcY);
+
+      await app.show();
+      file_content = document.getElementById("file_content");
+
+      if (args.length >= 2) {
+        read_file(args[1]);
+      }
+
+    }
   }
-}
+})();
 
 function exit_on_esc() {
   if (event.keyCode === 27) clean_exit();
